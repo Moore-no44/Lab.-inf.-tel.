347436c1474fc2c0d36f7632428f2ba539bd2c87
moore-no44 <morenotomaghelli44@gmail.com>
2023-04-28 22:16:37 +0200

    added complex mean

--- mean.h
+++ mean.h
@@ -1,3 +1,15 @@
 #pragma once
 
+typedef struct complex_int {
+	int re;
+	int im;
+} complex_int_t;
+
+typedef struct complex_float {
+	float re;
+	float im;
+} complex_float_t;
+
 float mean(int a, int b);
+
+complex_float_t complex_mean(complex_int_t a, complex_int_t b);
